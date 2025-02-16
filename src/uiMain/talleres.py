261de6c19e4faed96e9ad2d3592c428119d8be26
Andrés Felipe Muñoz Ortiz <163070299+Andrewxf3@users.noreply.github.com>
2025-02-15 21:22:40 -0500

    dlnt

--- src/uiMain/talleres.py
+++ src/uiMain/talleres.py
@@ -6,4 +6,22 @@ class talleres(Frame):
         self.pack()
         self.create_widgets()
     def create_widgets(self):
-        self.hola = Label(self)
+        self.frame1 = Frame(self, bg="cyan")
+        self.frame1.pack(expand=True, fill="both")
+        self.frame1.config(cursor="pirate", relief="sunken")
+
+        self.frame2 = Frame(self, bg="green")
+        self.frame2.pack(expand=True, fill="both")
+        self.frame2.config(cursor="hearth", relief="sunken")
+
+        self.hola = Label(self.frame1)
+        self.hola["text"] = "Reservaciones de talleres y actividades"
+        self.hola.pack(side="top")
+
+        self.holak = Label(self.frame2)
+        self.holak["text"] = " Aquí puede reservar rutas con talleres y actividades emocionantes"
+        self.holak.pack(side="top")
+        
+        self.field = Button(self.frame2)
+        self.field["text"] = "Reservar actividades"
+        self.field.pack()
